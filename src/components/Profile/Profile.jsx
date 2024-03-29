be0034c12adddbb454703bd323e7e52c48e5b7e3
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-03-30 00:00:22 +0200

    init

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import css from "./Profile.module.css";
+
+export default function Profile({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={css.container}>
+      <div className={css.infoCard}>
+        <img className={css.image} src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
